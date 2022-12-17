81771145debb589f5084f64c4fc11b00213e0234
Pranto Chakraborty <chakrabortypranto39@gmail.com>
2022-12-17 23:42:56 +0600

    get all users from another api

--- index.js
+++ index.js
@@ -11,6 +11,27 @@ app.use(express.json())
 import fetch from 'node-fetch';
 
 
+app.get('/users', async function (req, res) {
+    const url =
+        'https://jsonplaceholder.typicode.com/users';
+    const options = {
+        method: 'GET',
+    };
+    // promise syntax
+    fetch(url, options)
+        .then(res => res.json())
+        .then(json => console.log(json))
+        .catch(err => console.error('error:' + err));
+    try {
+        let response = await fetch(url, options);
+        response = await response.json();
+        res.status(200).json(response);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ msg: `Internal Server Error.` });
+    }
+});
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
